d0392b391e7c55b44da49eab0e0d0bd240132be9
Halil İLHAN <47319650+hybrayhem@users.noreply.github.com>
2023-12-14 00:50:53 +0300

    create simulation package

---
+++ src/brachiograph_simulation/src/joint_controller.cpp
@@ -0,0 +1,48 @@
+#include <ros/ros.h>
+#include <std_msgs/Float64.h>
+
+struct PositionCommand {
+    double joint1;
+    double joint2;
+    double joint3;
+};
+
+double deg2rad(double degrees) {
+    return degrees * M_PI / 180.0;
+}
+
+void publishJointPosition(ros::Publisher pub, double degrees) {
+    std_msgs::Float64 msg;
+    msg.data = deg2rad(degrees); // convert to radians
+    pub.publish(msg);
+}
+
+int main(int argc, char **argv)
+{
+    PositionCommand command;
+
+    // initialize as node
+    ros::init(argc, argv, "brachiograph_controller");
+    ros::NodeHandle nh;
+
+    // create publisher
+    ros::Publisher pub_joint1 = nh.advertise<std_msgs::Float64>("brachiograph/joint1_position_controller/command", 1000);
+    ros::Publisher pub_joint2 = nh.advertise<std_msgs::Float64>("brachiograph/joint2_position_controller/command", 1000);
+    ros::Publisher pub_joint3 = nh.advertise<std_msgs::Float64>("brachiograph/joint3_position_controller/command", 1000);
+
+    // publish loop at 10Hz
+    ros::Rate rate(10);
+    while(ros::ok()) {
+        // publishJointPosition(pub_joint1, command.joint1);
+        // publishJointPosition(pub_joint2, command.joint2);
+        // publishJointPosition(pub_joint3, command.joint3);
+        publishJointPosition(pub_joint1, 60);
+        publishJointPosition(pub_joint2, 60);
+        publishJointPosition(pub_joint3, 60);
+
+        ros::spinOnce(); // let ROS process incoming messages
+        rate.sleep(); // sleep until the next cycle
+    }
+
+    return 0;
+}
