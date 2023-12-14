962f904ae755fefad6f7929c6eb455fcd2b41135
Halil İLHAN <47319650+hybrayhem@users.noreply.github.com>
2023-12-14 16:48:12 +0300

    read positions with another thread

--- src/brachiograph_simulation/src/joint_controller.cpp
+++ src/brachiograph_simulation/src/joint_controller.cpp
@@ -4,6 +4,8 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <signal.h>
+#include <atomic>
+#include <thread>
 // ROS
 #include <ros/ros.h>
 #include <std_msgs/Float64.h>
@@ -17,22 +19,29 @@ struct PositionCommand {
  // FIFO
 int fifoFd;
 const char* fifoPath = "/tmp/brachiograph_fifo";
+std::thread reader;
+std::atomic<bool> newDataAvailable(false);
 void handle_sigint(int);
-void readPositionCommand(PositionCommand&);
+bool readPositionCommand(PositionCommand&);
+void readThread();
 // ROS
+PositionCommand command;
 double deg2rad(double);
 void publishJointPosition(ros::Publisher, double);
 
 
 int main(int argc, char **argv)
 {
+    std::cout << "Joint Controller starting..." << std::endl;
+
 // FIFO
-    int fifoFd = open(fifoPath, O_RDONLY);
+    int fifoFd = open(fifoPath, O_RDONLY | O_NONBLOCK);
     if (fifoFd == -1) {
         std::cerr << "Error opening FIFO for reading" << std::endl;
         return 1;
     }
     signal(SIGINT, handle_sigint);
+    reader = std::thread(readThread);
 
 // ROS
     // initialize as node
@@ -44,15 +53,18 @@ int main(int argc, char **argv)
     ros::Publisher pub_joint2 = nh.advertise<std_msgs::Float64>("brachiograph/joint2_position_controller/command", 1000);
     ros::Publisher pub_joint3 = nh.advertise<std_msgs::Float64>("brachiograph/joint3_position_controller/command", 1000);
 
+    std::cout << "Joint Controller started" << std::endl;
     // publish loop at 10Hz
-    PositionCommand command;
     ros::Rate rate(10);
+    // while(true) {
     while(ros::ok()) {
-        readPositionCommand(command);
-        
-        publishJointPosition(pub_joint1, command.joint1);
-        publishJointPosition(pub_joint2, command.joint2);
-        publishJointPosition(pub_joint3, command.joint3);
+        std::cout << "Looping..." << std::endl;
+        if (newDataAvailable.load()) {
+            publishJointPosition(pub_joint1, command.joint1);
+            publishJointPosition(pub_joint2, command.joint2);
+            publishJointPosition(pub_joint3, command.joint3);
+            newDataAvailable.store(false);
+        }
 
         ros::spinOnce(); // let ROS process incoming messages
         rate.sleep(); // sleep until the next cycle
@@ -74,20 +86,35 @@ void publishJointPosition(ros::Publisher pub, double degrees) {
 }
 
 // FIFO
-void readPositionCommand(PositionCommand& command) {
+void readThread() {
+    PositionCommand tempCommand;
+    while (true) {
+        if (readPositionCommand(tempCommand)) {
+            command = tempCommand;
+            newDataAvailable.store(true);
+        }
+    }
+}
+
+bool readPositionCommand(PositionCommand& command) {
     ssize_t bytesRead = read(fifoFd, &command, sizeof(command));
     if (bytesRead == -1) {
         std::cerr << "Error reading from FIFO" << std::endl;
         exit(1);
     }
 
+    if (bytesRead == 0) return false;
+
     std::cout << "Received PositionCommand: " 
             << command.joint1 << ", " 
             << command.joint2 << ", " 
             << command.joint3 << std::endl;
+
+    return true;
 }
 
 void handle_sigint(int sig) {
+    reader.detach();
     close(fifoFd);
     exit(0);
 }
