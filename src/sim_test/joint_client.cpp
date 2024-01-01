f37046db676c25fe23ec84cd699cb2d3fe01831e
Halil İLHAN <47319650+hybrayhem@users.noreply.github.com>
2024-01-01 22:18:43 +0300

    change server as receiver, client as sender

--- src/sim_test/joint_client.cpp
+++ src/sim_test/joint_client.cpp
@@ -6,72 +6,106 @@
 #include <string.h>
 #include <arpa/inet.h>
 
+// Structs
 struct PositionCommand {
     double joint1;
     double joint2;
     double joint3;
 };
 
+struct JointDirection {
+    int direction1;
+    int direction2;
+    int direction3;
+};
+
+// Constants
+const double safety_margin = 5; // degrees
+const double joint1_upper_limit = 68.75 - safety_margin;      // 1.20 radians
+const double joint2_upper_limit = 74.48 - safety_margin * 4;  // 1.30 radians
+const double joint3_upper_limit = 0;                          // 0 radians
+const double joint1_lower_limit = -180 + safety_margin * 14; // -3.14 radians
+const double joint2_lower_limit = -51.5 + safety_margin;     // -0.90 radians
+const double joint3_lower_limit = -63;                       // -1.10 radians (if collides with ground, set to -1.00)
+
+// Socket
 int PORT = 8080;
 char* IP_ADDRESS = "127.0.0.1"; // default values
-int sock = 0;
-struct sockaddr_in serv_addr;
+
+int clientSocket;
 void handle_sigint(int);
+PositionCommand nextMessage(PositionCommand, JointDirection&);
 
-
+// Main
 int main(int argc, char *argv[]) {
+    // Signal handlers
+    signal(SIGINT, handle_sigint);
+
+    // Command-line aarguments
     if (argc != 3) {
         std::cerr << "Usage: " << argv[0] << " <ip_address> <port>\n" << std::endl;
     } else {
         IP_ADDRESS = argv[1];
         PORT = std::atoi(argv[2]);
     }
-
-    std::cerr << "Starting at " << IP_ADDRESS << ":" << PORT << std::endl;
-
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        std::cerr << "Socket creation error" << std::endl;
-        return -1;
+    std::cerr << "Connecting to " << IP_ADDRESS << ":" << PORT << std::endl;
+    
+    clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (clientSocket == -1) {
+        std::cerr << "Error creating socket" << std::endl;
+        return 1;
     }
 
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(PORT);
-
-    if(inet_pton(AF_INET, IP_ADDRESS, &serv_addr.sin_addr) <= 0) {
+    sockaddr_in serverAddress;
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = htons(PORT);
+    
+    // Construct serverAddress from IP_ADDRESS
+    if(inet_pton(AF_INET, IP_ADDRESS, &serverAddress.sin_addr) <= 0) { 
         std::cerr << "Invalid address / Address not supported" << std::endl;
         return -1;
     }
 
-    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
-        std::cerr << "Connection failed" << std::endl;
-        return -1;
+    if (connect(clientSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) == -1) {
+        std::cerr << "Error connecting to server" << std::endl;
+        return 1;
     }
-    // while (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
-    //     std::cerr << "Connection failed, retrying..." << std::endl;
-    //     sleep(1); // wait for a second
-    // }
-    // std::cout << "Connected to the server" << std::endl;
 
-    signal(SIGINT, handle_sigint);
-
-    PositionCommand positionCommand;
+    PositionCommand message = {0, 0, 0};
+    JointDirection direction = {1, 1, 1};
     while (true) {
-        ssize_t bytesRead = read(sock, &positionCommand, sizeof(positionCommand));
-        if (bytesRead == -1) {
-            std::cerr << "Error reading from socket" << std::endl;
-            break;
-        }
+        send(clientSocket, &message, sizeof(message), 0);
+        usleep(100000); // 100ms
 
-        std::cout << "Received PositionCommand: " 
-                  << positionCommand.joint1 << ", " 
-                  << positionCommand.joint2 << ", " 
-                  << positionCommand.joint3 << std::endl;
+        std::cout << "PositionCommand Sent: " 
+            << message.joint1 << ", " 
+            << message.joint2 << ", " 
+            << message.joint3 << std::endl;
+
+        message = nextMessage(message, direction);
     }
 
     return 0;
 }
 
+// Implementation
 void handle_sigint(int sig) {
-    close(sock);
+    close(clientSocket);
     exit(0);
+}
+
+PositionCommand nextMessage(PositionCommand message, JointDirection& direction) {
+    if (message.joint1 >= joint1_upper_limit) direction.direction1 = -1;
+    if (message.joint2 >= joint2_upper_limit) direction.direction2 = -1;
+    if (message.joint3 >= joint3_upper_limit) direction.direction3 = -1;
+
+    if (message.joint1 <= joint1_lower_limit) direction.direction1 = 1;
+    if (message.joint2 <= joint2_lower_limit) direction.direction2 = 1;
+    if (message.joint3 <= joint3_lower_limit) direction.direction3 = 1;
+
+    message.joint1 += (double) direction.direction1;
+    message.joint2 += (double) direction.direction2;
+    message.joint3 += (double) direction.direction3;
+
+    return message;
 }
--- src/sim_test/joint_server.cpp
+++ src/sim_test/joint_server.cpp
@@ -6,122 +6,78 @@
 #include <string.h>
 #include <arpa/inet.h>
 
-// Structs
 struct PositionCommand {
     double joint1;
     double joint2;
     double joint3;
 };
 
-struct JointDirection {
-    int direction1;
-    int direction2;
-    int direction3;
-};
-
-// Functions
+int PORT = 8080;
+int serverSocket;
 void handle_sigint(int);
-PositionCommand nextMessage(PositionCommand, JointDirection&);
 
-// Constants
-const double safety_margin = 5; // degrees
-const double joint1_upper_limit = 68.75 - safety_margin;      // 1.20 radians
-const double joint2_upper_limit = 74.48 - safety_margin * 4;  // 1.30 radians
-const double joint3_upper_limit = 0;                          // 0 radians
-const double joint1_lower_limit = -180 + safety_margin * 14; // -3.14 radians
-const double joint2_lower_limit = -51.5 + safety_margin;     // -0.90 radians
-const double joint3_lower_limit = -63;                       // -1.10 radians (if collides with ground, set to -1.00)
-
-const int PORT = 8080;
-int serverFd;
-struct sockaddr_in address;
-int opt = 1;
-int addrlen = sizeof(address);
-
-// Main
 int main() {
-    if ((serverFd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
-        std::cerr << "Socket failed" << std::endl;
-        exit(EXIT_FAILURE);
-    }
-
-    if (setsockopt(serverFd, SOL_SOCKET, SO_REUSEADDR /*| SO_REUSEPORT*/, &opt, sizeof(opt))) {
-        std::cerr << "Setsockopt failed with error: " << strerror(errno) << std::endl;
-        exit(EXIT_FAILURE);
-    }
-
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(PORT);
-
-    if (bind(serverFd, (struct sockaddr *)&address, sizeof(address)) < 0) {
-        std::cerr << "Bind failed" << std::endl;
-        exit(EXIT_FAILURE);
-    }
-
-    if (listen(serverFd, 3) < 0) {
-        std::cerr << "Listen failed" << std::endl;
-        exit(EXIT_FAILURE);
-    }
-
-    int newSocket;
-    if ((newSocket = accept(serverFd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
-        std::cerr << "Accept failed" << std::endl;
-        exit(EXIT_FAILURE);
-    }
-
-    // Print the server address
-    char ipstr[INET6_ADDRSTRLEN];
-    int port;
-    if (address.sin_family == AF_INET) {
-        struct sockaddr_in *s = (struct sockaddr_in *)&address;
-        port = ntohs(s->sin_port);
-        inet_ntop(AF_INET, &s->sin_addr, ipstr, sizeof ipstr);
-    } else { // AF_INET6
-        struct sockaddr_in6 *s = (struct sockaddr_in6 *)&address;
-        port = ntohs(s->sin6_port);
-        inet_ntop(AF_INET6, &s->sin6_addr, ipstr, sizeof ipstr);
-    }
-    std::cout << "Server address: " << ipstr << ":" << port << std::endl;
-
-
+    // Signal handlers
     signal(SIGINT, handle_sigint);
 
-    PositionCommand message = {0, 0, 0};
-    JointDirection direction = {1, 1, 1};
+    if ((serverSocket = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        std::cerr << "Socket creation error" << std::endl;
+        return -1;
+    }
+
+    sockaddr_in serverAddress;
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = htons(PORT);
+    serverAddress.sin_addr.s_addr = INADDR_ANY;
+    
+    if (bind(serverSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) < 0) {
+        std::cerr << "Error binding socket" << std::endl;
+        return -1;
+    }
+
+    if (listen(serverSocket, 1) < 0) {
+        std::cerr << "Error listening on socket" << std::endl;
+        return -1;
+    }
+
     while (true) {
-        send(newSocket, &message, sizeof(message), 0);
-        usleep(100000); // 100ms
+        int clientSocket = accept(serverSocket, NULL, NULL);
+        if (clientSocket < 0) {
+            std::cerr << "Accepting client failed, retrying..." << std::endl;
+            return -1;
+        }
 
-        std::cout << "PositionCommand Sent: " 
-            << message.joint1 << ", " 
-            << message.joint2 << ", " 
-            << message.joint3 << std::endl;
+        // Print the server address for debug
+        char ipstr[INET_ADDRSTRLEN];
+        int port = ntohs(serverAddress.sin_port);
+        inet_ntop(AF_INET, &serverAddress.sin_addr, ipstr, INET6_ADDRSTRLEN);
+        std::cout << "Server running at: " << ipstr << ":" << port << std::endl;
 
-        message = nextMessage(message, direction);
+        while (true) {
+            PositionCommand positionCommand;
+
+            ssize_t bytesRead = recv(clientSocket, &positionCommand, sizeof(positionCommand), 0);
+            if (bytesRead == 0) {
+                std::cout << "Client disconnected" << std::endl;
+                break;
+            } else if (bytesRead == -1) {
+                std::cerr << "Error reading from client" << std::endl;
+                break;
+            }
+
+            std::cout << "Received PositionCommand: " 
+                    << positionCommand.joint1 << ", " 
+                    << positionCommand.joint2 << ", " 
+                    << positionCommand.joint3 << std::endl;
+        }
+        
+        close(clientSocket);
     }
 
     return 0;
 }
 
-// Implementation
 void handle_sigint(int sig) {
-    close(serverFd);
+    close(serverSocket);
     exit(0);
-}
-
-PositionCommand nextMessage(PositionCommand message, JointDirection& direction) {
-    if (message.joint1 >= joint1_upper_limit) direction.direction1 = -1;
-    if (message.joint2 >= joint2_upper_limit) direction.direction2 = -1;
-    if (message.joint3 >= joint3_upper_limit) direction.direction3 = -1;
-
-    if (message.joint1 <= joint1_lower_limit) direction.direction1 = 1;
-    if (message.joint2 <= joint2_lower_limit) direction.direction2 = 1;
-    if (message.joint3 <= joint3_lower_limit) direction.direction3 = 1;
-
-    message.joint1 += (double) direction.direction1;
-    message.joint2 += (double) direction.direction2;
-    message.joint3 += (double) direction.direction3;
-
-    return message;
 }
